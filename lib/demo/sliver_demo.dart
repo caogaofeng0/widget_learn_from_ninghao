9689bc35b77c595b7ceb2e6275b1b102316133aa
caogf <caogf@guahao.com>
2019-07-20 17:06:14 +0800

    sliver init

---
+++ lib/demo/sliver_demo.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import '../model/post.dart';
+
+class SliverDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: <Widget>[
+         SliverGrid(
+           gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+             crossAxisCount: 2,
+             crossAxisSpacing: 8.0,
+             mainAxisSpacing: 8.0,
+             childAspectRatio: 1.0
+           ),
+           childCount: posts.length
+           delegate: SliverChildBuilderDelegate(BuildContext context, int index) {
+             return Container(
+               child: Image.network(
+                 posts[index].imageUrl,
+                 fit: BoxFit.cover,
+               ),
+             );
+           },
+         )
+        ],
+      ),
+    );
+  }
+}
