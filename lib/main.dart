aadc65c8fd613ee19b68528190dfd21b3cf03bbd
caogf <caogf@guahao.com>
2019-07-20 17:06:35 +0800

    sliver init

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,14 @@ import './demo/listview_demo.dart';
 import './demo/basic_demo.dart';
 import './demo/layout_demo.dart';
 import './demo/view_demo.dart';
-
+import './demo/sliver_demo.dart';
 void main() => runApp(App());
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Home(),
+      home: SliverDemo(),
       theme: ThemeData(
         primarySwatch: Colors.yellow,
         highlightColor: Color.fromRGBO(255, 255, 255, 0.5),
