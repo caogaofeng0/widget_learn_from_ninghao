e8eec4152c5e4852d76fc2fc7ad2b621f6ba646d
caogf <caogf@guahao.com>
2019-07-20 14:01:37 +0800

    添加 drawer

---
+++ lib/demo/bottom_navigation_bar_demo.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class BottomNavigationBarDemo extends StatefulWidget {
+  @override
+  _BottomNavigationBarDemoState createState() => _BottomNavigationBarDemoState();
+}
+
+class _BottomNavigationBarDemoState extends State<BottomNavigationBarDemo> {
+  int _currentIndex = 0;
+  void _onTapHandler (int index) {
+    setState;
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: _onTapHandler,
+          type: BottomNavigationBarType.fixed,
+          fixedColor: Colors.black,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.explore),
+              title: Text('explore')
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.history),
+              title: Text('history')
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.list),
+              title: Text('list')
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              title: Text('My')
+            )
+          ],
+        );
+  }
+}
---
+++ lib/demo/drawer.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class DrawerDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          UserAccountsDrawerHeader(
+            accountName:
+                Text('mei xi', style: TextStyle(fontWeight: FontWeight.bold)),
+            accountEmail: Text('data@126.com'),
+            currentAccountPicture: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'http://www.aihuahua.net/uploads/allimg/141228/1-14122Q40251937.jpg'),
+            ),
+            decoration: BoxDecoration(
+              color: Colors.yellow[400],
+              image: DecorationImage(
+                  image: NetworkImage(
+                      'https://cn.bing.com/th?id=OIP.FcY_x6VOUV1CDfkzefdphQHaEo&pid=Api&rs=1'),
+                  fit: BoxFit.cover,
+                  colorFilter: ColorFilter.mode(
+                      Colors.yellow[400].withOpacity(0.6), BlendMode.srcOver)),
+            ),
+          ),
+          ListTile(
+            onLongPress: () => Navigator.pop(context),
+            title: Text('xiao ming', textAlign: TextAlign.end),
+            trailing: Icon(Icons.message, color: Colors.black12),
+          ),
+          ListTile(
+            onLongPress: () => Navigator.pop(context),
+            title: Text('xiao li', textAlign: TextAlign.end),
+            trailing: Icon(Icons.message, color: Colors.black12),
+          ),
+          ListTile(
+            onTap: () => Navigator.pop(context),
+            title: Text('xiao wang', textAlign: TextAlign.end),
+            trailing: Icon(Icons.message, color: Colors.black12),
+          )
+        ],
+      ),
+    );
+  }
+}
